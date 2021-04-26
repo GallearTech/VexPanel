4e8428c5573a46383d7fa9d03f56993d036db2a8
The-MG <63264652+The-MG@users.noreply.github.com>
2021-04-26 12:57:35 -0700

    Update Version, Fix Coin Earning

--- inc/coins.php
+++ inc/coins.php
@@ -19,10 +19,10 @@ if( checklogin() == true ) {
 }
 
 if ($idlecheck <= $currenttimestamp) {
-$conn->query("UPDATE users SET coins='".mysqli_real_escape_string($conn, $coins + 1)."' WHERE discord_id-'".mysqli_real_escape_string($conn, $user->id)."'");
-$conn->query("UPDATE users SET minutes_idle='".mysqli_real_escape_string($conn, $idlemins + 1)."' WHERE discord_id='".mysqli_real_escape_string($conn, $user->id)."'");
-$conn->query("UPDATE users SET last_seen='".mysqli_real_escape_string($conn, $currenttimestamp)."' WHERE discord_id='".mysqli_real_escape_string($conn, $user->id)."'");
-header("location: ../idle.php");
+    $conn->query("UPDATE users SET coins='".mysqli_real_escape_string($conn, $coins + 1)."' WHERE discord_id='".mysqli_real_escape_string($conn, $user->id)."'");
+    $conn->query("UPDATE users SET minutes_idle='".mysqli_real_escape_string($conn, $idlemins + 1)."' WHERE discord_id='".mysqli_real_escape_string($conn, $user->id)."'");
+    $conn->query("UPDATE users SET last_seen='".mysqli_real_escape_string($conn, $currenttimestamp)."' WHERE discord_id='".mysqli_real_escape_string($conn, $user->id)."'");
+    header("location: ../idle.php");
 }else{
     die("Please don't try to get more coins by exploiting!");
 }
