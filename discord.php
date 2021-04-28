3496c057ab6da3608372bfb82aa26d33835be095
The-MG <63264652+The-MG@users.noreply.github.com>
2021-04-28 08:14:16 -0700

    Fix login discord log

--- discord.php
+++ discord.php
@@ -70,11 +70,11 @@ if(session('access_token')) {
             'last_name' => 'User'
         ]);
         $conn->query("INSERT INTO users (discord_user, discord_id, discord_email, ptero_user, ptero_pwd, ptero_uid, signup_ip, last_ip, minutes_idle, coins, last_seen, user_roles, ram, cpu, disk_space, server_slots) VALUES ('".mysqli_real_escape_string($conn, $user->username)."', '".mysqli_real_escape_string($conn, $user->id)."', '".mysqli_real_escape_string($conn, $user->email)."', '".mysqli_real_escape_string($conn, $ptero_user)."', '".mysqli_real_escape_string($conn, $ptero_pwd)."', '".mysqli_real_escape_string($conn, $ptuser->id)."', '".mysqli_real_escape_string($conn, $userip)."', '".mysqli_real_escape_string($conn, $userip)."', '0', '0', '".mysqli_real_escape_string($conn, $t)."', '0', '0', '0', '0', '0')");
-        if($discordLog === 'true'){
-            LoginLog($discord_log);
-        }
         $_SESSION['discord_user'] = $user;
         $_SESSION['isLoggedIn'] = true;
+        if($discordLog === 'true'){
+          LoginLog($discord_log);
+      }
         header("location: ./");
         die();
     } catch(\HCGCloud\Pterodactyl\Exceptions\ValidationException $e){
@@ -82,11 +82,11 @@ if(session('access_token')) {
     }
   }else{
     JoinGuild(session('access_token'), $guildID, $user->id, $discordBotToken);
+    $_SESSION['discord_user'] = $user;
+    $_SESSION['isLoggedIn'] = true;
     if($discordLog === 'true'){
       LoginLog($discord_log);
     }
-    $_SESSION['discord_user'] = $user;
-    $_SESSION['isLoggedIn'] = true;
     header("location: ./");
     die();
   }
