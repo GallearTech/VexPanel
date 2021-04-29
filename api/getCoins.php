147b5484e9cf9bde6921d26375a281a80f777870
The-MG <63264652+The-MG@users.noreply.github.com>
2021-04-29 10:58:19 -0700

    Update getCoins.php

--- api/getCoins.php
+++ api/getCoins.php
@@ -1,7 +1,8 @@
 <?php
 require '../config.php';
 $userInfo = $conn->query("SELECT * FROM users WHERE discord_id='".$_GET['did']."'")->fetch_assoc();
-if($userInfo->num_rows = 0){
+$checkDB = $conn->query("SELECT * FROM users WHERE discord_id='".$_GET['did']."'");
+if($checkDB->num_rows == 0){
     echo '`**USER NOT FOUND**`';
 }
 $userCoins = $userInfo['coins'];
