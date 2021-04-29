9d758c6a52c8e41cafcbf44bd4221a19f6a8d24a
The-MG <63264652+The-MG@users.noreply.github.com>
2021-04-29 13:59:29 -0700

    Add isIdle api for bot

---
+++ api/isIdle.php
@@ -0,0 +1,17 @@
+<?php
+require '../config.php';
+$userInfo = $conn->query("SELECT * FROM users WHERE discord_id='".$_GET['did']."'")->fetch_assoc();
+$checkDB = $conn->query("SELECT * FROM users WHERE discord_id='".$_GET['did']."'");
+if($checkDB->num_rows == 0){
+    echo '**USER NOT FOUND**';
+}
+$userLastSeen = $userInfo['last_seen'];
+$idlecheck = $userLastSeen + 60;
+
+$currenttime = new DateTime();
+$currenttimestamp = $currenttime->getTimestamp();
+if($idlecheck >= $currenttimestamp){
+echo 'are';
+}else{
+    echo 'are not';
+}
